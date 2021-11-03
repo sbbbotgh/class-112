2615ca8945869d304abae5dead5a92801a04d880
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2021-11-03 14:41:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -41,6 +41,30 @@ function gotResult(error,results){
         console.error(error);
     } else{
         console.log(results);
-        //document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label=="happy"){
+            document.getElementById("update_emoji").innerHTML = "&#128522;";
+        }
+        else if(results[0].label=="sad"){
+            document.getElementById("update_emoji").innerHTML = "&#128532;";
+        }
+        else if(results[0].label == "angry"){
+            document.getElementById("update_emoji").innerHTML = "&#128548;";
+        }
+
+
+        if(results[1].label=="happy"){
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";
+        }
+        else if(results[1].label=="sad"){
+            document.getElementById("update_emoji").innerHTML = "&#128532;";
+        }
+        else if (results[1].label == "angry"){
+            document.getElementById("update_emoji").innerHTML = "&#128548;";
+        }
     }
 }
